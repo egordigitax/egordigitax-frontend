94f4f73c649b71e56af1b6a371f105187d830728
egordigitax <66309908+egordigitax@users.noreply.github.com>
2024-02-28 16:51:06 +0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -23,13 +23,6 @@ const { locale } = useI18n()
 const router = useRouter();
 
 onBeforeMount(() => {
-  let telegramApi = document.createElement('script')
-  telegramApi.setAttribute('src', 'https://telegram.org/js/telegram-web-app.js')
-  document.head.appendChild(telegramApi)
-
-  let tg = window.Telegram.WebApp;
-  tg.expand()
-
   const localLocale = localStorage.getItem('locale');
   const systemLocale = navigator.language;
   if (localLocale) {
